5caa1ee2f1a702bbc3e217846f73a7acbb979e5f
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-09-26 12:08:41 -0300

    finalizando estilo e funcao calc

--- js/script.js
+++ js/script.js
@@ -5,5 +5,13 @@ const button = document.getElementById('btn').addEventListener('click', function
   var verbal = Number(document.getElementById('verbal').value);
   var visual = Number(document.getElementById('visual').value);
   var calcResult = (reaction + memory + verbal + visual) / 4;
-  document.getElementById('result').innerHTML = calcResult;
-})
+  if(calcResult >= 65) {
+    document.getElementById('result').innerHTML = calcResult;
+    document.getElementById('result-title').innerHTML = "Great";
+    document.getElementById('result-description').innerHTML = "Your scored higher then 65% of the people who have taken these tests.";
+  } else {
+    document.getElementById('result').innerHTML = calcResult;
+    document.getElementById('result-title').innerHTML = "";
+    document.getElementById('result-description').innerHTML = "";
+  }
+});
